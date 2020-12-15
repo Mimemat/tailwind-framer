987d1947feb08db751392d190fc7dcd5ae04f691
Mimemat <hyatomimemat@gmail.com>
2020-12-15 20:50:26 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-auto flex bg-gray-900 justify-center items-center">
+      <h1 className="text-gray-300 font-bold text-5xl">
+        Hello world
+      </h1>
+    </div>
+  )
+}
